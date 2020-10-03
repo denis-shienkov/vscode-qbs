53a3a932bc293adfc827f94c7b2cc6ddabf149e2
Denis Shienkov <denis.shienkov@gmail.com>
2020-10-03 16:54:11 +0300

    Add missing event for 'configure' command

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,22 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "qbs-tools" is now active!');
 
-    let configureCommand = vscode.commands.registerCommand('qbs.configure', () => {
-        // Display a message box to the user
+    const configureCmd = vscode.commands.registerCommand('qbs.configure', () => {
         vscode.window.showInformationMessage('QBS: configure');
     });
-    context.subscriptions.push(configureCommand);
+    context.subscriptions.push(configureCmd);
 
-    let buildCommand = vscode.commands.registerCommand('qbs.build', () => {
-        // Display a message box to the user
+    const buildCmd = vscode.commands.registerCommand('qbs.build', () => {
         vscode.window.showInformationMessage('QBS: build');
     });
-    context.subscriptions.push(buildCommand);
+    context.subscriptions.push(buildCmd);
 
-    let cleanCommand = vscode.commands.registerCommand('qbs.clean', () => {
-        // Display a message box to the user
+    const cleanCmd = vscode.commands.registerCommand('qbs.clean', () => {
         vscode.window.showInformationMessage('QBS: clean');
     });
-    context.subscriptions.push(cleanCommand);
+    context.subscriptions.push(cleanCmd);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
