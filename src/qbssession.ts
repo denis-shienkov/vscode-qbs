266f2e8a4f43ffa7a22c14062285099cb2c1c07a
Denis Shienkov <denis.shienkov@gmail.com>
2020-10-05 22:05:51 +0300

    Add QBS session status button

--- src/qbssession.ts
+++ src/qbssession.ts
@@ -1,11 +1,21 @@
 import * as vscode from 'vscode';
 
+export enum QbsSessionStatus {
+    Stopped,
+    Started,
+    Stopping,
+    Starting
+  }
+
 export class QbsSession implements vscode.Disposable {
     // Private members.
+    private _status: QbsSessionStatus = QbsSessionStatus.Stopped;
     private _projectUri!: vscode.Uri; // Current project *.qbs file.
+    private _onStatusChanged: vscode.EventEmitter<QbsSessionStatus> = new vscode.EventEmitter<QbsSessionStatus>();
     private _onProjectUriChanged: vscode.EventEmitter<vscode.Uri> = new vscode.EventEmitter<vscode.Uri>();
     
     // Public events.
+    readonly onStatusChanged: vscode.Event<QbsSessionStatus> = this._onStatusChanged.event;
     readonly onProjectUriChanged: vscode.Event<vscode.Uri> = this._onProjectUriChanged.event;
 
     // Constructors.
@@ -25,6 +35,17 @@ export class QbsSession implements vscode.Disposable {
     dispose() {  }
 
     // Public methods.
+
+    set status(st: QbsSessionStatus) {
+        if (st === this._status)
+            return;
+        this._status = st;
+        this._onStatusChanged.fire(this._status);
+    }
+
+    get status(): QbsSessionStatus {
+        return this._status;
+    }
 
     set projectUri(uri: vscode.Uri) {
         if (uri === this._projectUri)
--- src/statusbar.ts
+++ src/statusbar.ts
@@ -3,16 +3,23 @@ import * as nls from 'vscode-nls';
 
 import { basename } from 'path'; 
 
-import {QbsSession} from './qbssession';
+import {QbsSession, QbsSessionStatus} from './qbssession';
 
 const localize: nls.LocalizeFunc = nls.loadMessageBundle();
 
 export class StatusBar implements vscode.Disposable {
     // Private members.
+    private _statusButton: vscode.StatusBarItem;
     private _projectFileButton: vscode.StatusBarItem;
 
     // Constructors.
     constructor(private readonly _session: QbsSession) {
+        // Create the QBS session status button.
+        this._statusButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+        this._statusButton.tooltip = localize('qbs.active.project.select.tooltip', 'QBS session status');
+        this._statusButton.show();
+
+        // Create the QBS project file selection button.
         this._projectFileButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
         this._projectFileButton.tooltip = localize('qbs.active.project.select.tooltip', 'Click to select the active project');
         this._projectFileButton.command = 'qbs.selectProject';
@@ -21,6 +28,9 @@ export class StatusBar implements vscode.Disposable {
         // Subscribe on the session events.
         _session.onProjectUriChanged(uri => {
             this.updateProjectName(uri);
+        });
+        _session.onStatusChanged(status => {
+            this.updateSessionStatus(StatusBar.sessionStatusName(this._session.status));
         });
 
         this.initialize();
@@ -36,9 +46,30 @@ export class StatusBar implements vscode.Disposable {
     // Public overriden methods.
     dispose(): void { }
 
+    // Private static methods.
+
+    private static sessionStatusName(status: QbsSessionStatus) {
+        switch (status) {
+        case QbsSessionStatus.Started:
+            return localize('qbs.session.status.started', `started`);
+        case QbsSessionStatus.Starting:
+            return localize('qbs.session.status.starting', `starting`);
+        case QbsSessionStatus.Stopped:
+            return localize('qbs.session.status.stopped', `stopped`);
+        case QbsSessionStatus.Stopping:
+            return localize('qbs.session.status.stopping', `stopping`);
+        }
+    }
+
     // Private methods.
+
     private async initialize() {
+        await this.updateSessionStatus(StatusBar.sessionStatusName(this._session.status));
         await this.updateProjectName();
+    }
+
+    private async updateSessionStatus(status: string) {
+        this._statusButton.text = localize('qbs.session.status', `QBS (${status})`);
     }
 
     private async updateProjectName(uri?: vscode.Uri) {
