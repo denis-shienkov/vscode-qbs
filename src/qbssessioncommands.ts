b1c33342780a770f2c74f1880097f635cf972793
Denis Shienkov <denis.shienkov@gmail.com>
2020-10-11 15:43:51 +0300

    Split QBS sesison commands implementations

--- src/qbssessioncommands.ts
+++ src/qbssessioncommands.ts
@@ -5,53 +5,80 @@ import {QbsSession} from './qbssession';
 
 import * as QbsSelectors from './qbsselectors';
 
+// Private functions.
+
+async function startSession(session: QbsSession) {
+    await session.start();
+}
+
+async function stopSession(session: QbsSession) {
+    await session.stop();
+}
+
+async function selectProject(session: QbsSession) {
+    await QbsSelectors.selectProject().then(projectUri => {
+        if (projectUri) {
+            session.projectUri = projectUri;
+        }
+    });
+}
+
+async function selectProfile(session: QbsSession) {
+    await QbsSelectors.selectProfile().then(profileName => {
+        if (profileName) {
+            session.profileName = profileName;
+        }
+    });
+}
+
+async function selectConfiguration(session: QbsSession) {
+    await QbsSelectors.selectConfiguration().then(configurationName => {
+        if (configurationName) {
+            session.configurationName = configurationName;
+        }
+   });
+}
+
+async function resolve(session: QbsSession) {
+    await session.resolve();
+}
+
+async function build(session: QbsSession) {
+    await session.build();
+}
+
+async function clean(session: QbsSession) {
+    await session.clean();
+}
+
+// Public function.
+
 export async function subscribeCommands(ctx: vscode.ExtensionContext, session: QbsSession) {
-    const startSessionCmd = vscode.commands.registerCommand('qbs.startSession', () => {
-        session!.start();
-    });
-    ctx.subscriptions.push(startSessionCmd);
-
-    const stopSessionCmd = vscode.commands.registerCommand('qbs.stopSession', () => {
-        session!.stop();
-    });
-    ctx.subscriptions.push(stopSessionCmd);
-
-    const selectProjectCmd = vscode.commands.registerCommand('qbs.selectProject', () => {
-        QbsSelectors.selectProject().then(projectUri => {
-            if (projectUri)
-            session!.projectUri = projectUri;
-        });
-    });
-    ctx.subscriptions.push(selectProjectCmd);
-
-    const selectProfileCmd = vscode.commands.registerCommand('qbs.selectProfile', () => {
-        QbsSelectors.selectProfile().then(profileName => {
-            if (profileName)
-                session!.profileName = profileName;
-        });
-    });
-    ctx.subscriptions.push(selectProfileCmd);
-
-    const selectConfigurationCmd = vscode.commands.registerCommand('qbs.selectConfiguration', () => {
-        QbsSelectors.selectConfiguration().then(configurationName => {
-             if (configurationName)
-             session!.configurationName = configurationName;
-        });
-    });
-    ctx.subscriptions.push(selectConfigurationCmd);
-
-    const resolveCmd = vscode.commands.registerCommand('qbs.resolve', () => {
-        session!.resolve();
-    });
-    ctx.subscriptions.push(resolveCmd);
-
-    const buildCmd = vscode.commands.registerCommand('qbs.build', () => {
-        session!.build();
-    });
-    ctx.subscriptions.push(buildCmd);
-
-    const cleanCmd = vscode.commands.registerCommand('qbs.clean', () => {
-        session!.clean();
-    });
-    ctx.subscriptions.push(cleanCmd);
+    // Start/stop session commands.
+    ctx.subscriptions.push(vscode.commands.registerCommand('qbs.startSession', () => {
+        startSession(session);
+    }));
+    ctx.subscriptions.push(vscode.commands.registerCommand('qbs.stopSession', () => {
+        stopSession(session);
+    }));
+    // Session properties commands.
+    ctx.subscriptions.push(vscode.commands.registerCommand('qbs.selectProject', () => {
+        selectProject(session);
+    }));
+    ctx.subscriptions.push(vscode.commands.registerCommand('qbs.selectProfile', () => {
+        selectProfile(session);
+    }));
+    ctx.subscriptions.push(vscode.commands.registerCommand('qbs.selectConfiguration', () => {
+        selectConfiguration(session);
+    }));
+    // Session resolve/build/clean commands.
+    ctx.subscriptions.push(vscode.commands.registerCommand('qbs.resolve', () => {
+        resolve(session);
+    }));
+    ctx.subscriptions.push(vscode.commands.registerCommand('qbs.build', () => {
+        build(session);
+    }));
+    ctx.subscriptions.push(vscode.commands.registerCommand('qbs.clean', () => {
+        clean(session);
+    }));
 }
