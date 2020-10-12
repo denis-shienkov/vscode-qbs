03955950746f578a104c6e38c996efb5c3a29402
Denis Shienkov <denis.shienkov@gmail.com>
2020-10-12 17:18:15 +0300

    Improve code style a bit

--- src/qbsconfig.ts
+++ src/qbsconfig.ts
@@ -4,32 +4,36 @@ import * as vscode from 'vscode';
 import * as QbsUtils from './qbsutils';
 
 export function fetchQbsPath(): string {
-    const path = QbsUtils.expandPath(vscode.workspace.getConfiguration('qbs').get('qbsPath') as string) || '';
-    return path;
+    const v = vscode.workspace.getConfiguration('qbs').get('qbsPath') as string;
+    return QbsUtils.expandPath(v) || '';
 }
 
 export function fetchQbsSettingsDirectory(): string {
-    const path = QbsUtils.expandPath(vscode.workspace.getConfiguration('qbs').get('settingsDirectory') as string) || '';
-    return path;
+    const v = vscode.workspace.getConfiguration('qbs').get('settingsDirectory') as string;
+    return QbsUtils.expandPath(v) || '';
 }
 
 export function fetchQbsBuildDirectory(): string {
-    const path = QbsUtils.expandPath(vscode.workspace.getConfiguration('qbs').get('buildDirectory') as string) || '';
-    return path;
+    const v = vscode.workspace.getConfiguration('qbs').get('buildDirectory') as string;
+    return QbsUtils.expandPath(v) || '';
 }
 
-export function fetchQbsKeepGoing(): boolean  {
-    return vscode.workspace.getConfiguration('qbs').get('keepGoing') as boolean || false;
+export function fetchQbsKeepGoing(): boolean {
+    const v = vscode.workspace.getConfiguration('qbs').get('keepGoing') as boolean;
+    return v || false;
 }
 
-export function fetchQbsMaxJobs(): number  {
-    return vscode.workspace.getConfiguration('qbs').get('maxBuildJobs') as number || 0;
+export function fetchQbsMaxJobs(): number {
+    const v = vscode.workspace.getConfiguration('qbs').get('maxBuildJobs') as number;
+    return v || 0;
 }
 
-export function fetchQbsShowCommandLines(): boolean  {
-    return vscode.workspace.getConfiguration('qbs').get('showCommandLines') as boolean || false;
+export function fetchQbsShowCommandLines(): boolean {
+    const v = vscode.workspace.getConfiguration('qbs').get('showCommandLines') as boolean;
+    return v || false;
 }
 
-export function fetchQbsForceProbes(): boolean  {
-    return vscode.workspace.getConfiguration('qbs').get('forceProbes') as boolean || false;
+export function fetchQbsForceProbes(): boolean {
+    const v = vscode.workspace.getConfiguration('qbs').get('forceProbes') as boolean;
+    return v || false;
 }
--- src/qbsstatusbar.ts
+++ src/qbsstatusbar.ts
@@ -19,49 +19,48 @@ export class QbsStatusBar implements vscode.Disposable {
 
     // Constructors.
     constructor(private readonly _session: QbsSession) {
+        const alignment = vscode.StatusBarAlignment.Left;
+
         // Create the QBS session status button.
-        this._statusButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, -1);
+        this._statusButton = vscode.window.createStatusBarItem(alignment, -1);
         this._statusButton.tooltip = localize('qbs.status.tooltip', 'QBS session status');
         this._statusButton.show();
 
         // Create the QBS project file selection button.
-        this._projectButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, -2);
-        this._projectButton.tooltip = localize('qbs.active.project.select.tooltip', 'Click to select the active project');
+        this._projectButton = vscode.window.createStatusBarItem(alignment, -2);
+        this._projectButton.tooltip = localize('qbs.active.project.select.tooltip',
+                                               'Click to select the active project');
         this._projectButton.command = 'qbs.selectProject';
         this._projectButton.show();
 
         // Create the QBS build profile selection button.
-        this._profileButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, -3);
-        this._profileButton.tooltip = localize('qbs.build.profile.select.tooltip', 'Click to select the build profile');
+        this._profileButton = vscode.window.createStatusBarItem(alignment, -3);
+        this._profileButton.tooltip = localize('qbs.build.profile.select.tooltip',
+                                               'Click to select the build profile');
         this._profileButton.command = 'qbs.selectProfile';
         this._profileButton.show();
 
         // Create the QBS build configuration selection button.
-        this._configurationButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, -4);
-        this._configurationButton.tooltip = localize('qbs.build.configuration.select.tooltip', 'Click to select the build configuration');
+        this._configurationButton = vscode.window.createStatusBarItem(alignment, -4);
+        this._configurationButton.tooltip = localize('qbs.build.configuration.select.tooltip',
+                                                     'Click to select the build configuration');
         this._configurationButton.command = 'qbs.selectConfiguration';
         this._configurationButton.show();
 
         // Create the QBS build run button.
-        this._buildRunButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, -5);
+        this._buildRunButton = vscode.window.createStatusBarItem(alignment, -5);
         this._buildRunButton.text = localize('qbs.build.run', `$(gear) Build`);
-        this._buildRunButton.tooltip = localize('qbs.build.run.tooltip', 'Build the selected target');
+        this._buildRunButton.tooltip = localize('qbs.build.run.tooltip',
+                                                'Build the selected target');
         this._buildRunButton.command = 'qbs.build';
         this._buildRunButton.show();
 
         // Subscribe on the session events.
-        _session.onStatusChanged(status => {
-            this.updateSessionStatus(QbsUtils.sessionStatusName(this._session.status));
-        });
-        _session.onProjectUriChanged(uri => {
-            this.updateProjectFileName(uri);
-        });
-        _session.onProfileNameChanged(name => {
-            this.updateProfileName(name);
-        });
-        _session.onConfigurationNameChanged(name => {
-            this.updateConfigurationName(name);
-        });
+        _session.onStatusChanged(status => this.updateSessionStatus(
+            QbsUtils.sessionStatusName(this._session.status)));
+        _session.onProjectUriChanged(uri => this.updateProjectFileName(uri));
+        _session.onProfileNameChanged(name => this.updateProfileName(name));
+        _session.onConfigurationNameChanged(name => this.updateConfigurationName(name));
 
         this.initialize();
     }
@@ -79,7 +78,8 @@ export class QbsStatusBar implements vscode.Disposable {
     // Private methods.
 
     private async initialize() {
-        await this.updateSessionStatus(QbsUtils.sessionStatusName(this._session.status));
+        await this.updateSessionStatus(
+            QbsUtils.sessionStatusName(this._session.status));
         await this.updateProjectFileName();
         await this.updateProfileName();
         await this.updateConfigurationName();
@@ -90,17 +90,21 @@ export class QbsStatusBar implements vscode.Disposable {
     }
 
     private async updateProjectFileName(uri?: vscode.Uri) {
-        const projectName = uri ? basename(uri.fsPath) : localize('qbs.active.project.empty', 'empty');
-        this._projectButton.text = localize('qbs.active.project.select', `$(project) [${projectName}]`);
+        const text = uri ? basename(uri.fsPath)
+                         : localize('qbs.active.project.empty', 'empty');
+        this._projectButton.text = localize('qbs.active.project.select',
+                                            `$(project) [${text}]`);
     }
 
     private async updateProfileName(profile?: string) {
-        const profileName = profile ? profile : localize('qbs.active.profile.empty', 'none');
-        this._profileButton.text = localize('qbs.build.profile.select', `$(tools) [${profileName}]`);
+        const text = profile ? profile : localize('qbs.active.profile.empty', 'none');
+        this._profileButton.text = localize('qbs.build.profile.select',
+                                            `$(tools) [${text}]`);
     }
 
     private async updateConfigurationName(configuration?: string) {
-        const configurationName = configuration ? configuration : 'default';
-        this._configurationButton.text = localize('qbs.build.configuration.select', `$(settings) [${configurationName}]`);
+        const text = configuration ? configuration : 'default';
+        this._configurationButton.text = localize('qbs.build.configuration.select',
+                                                  `$(settings) [${text}]`);
     }
 }
