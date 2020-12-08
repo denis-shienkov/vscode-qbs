66c5f22e629d07e6953846b0fcffcbdc7b7c2a21
Denis Shienkov <denis.shienkov@gmail.com>
2020-12-08 12:38:51 +0300

    Improve 'msvc' problems parser a bit

--- src/diagnostic/qbsmsvcdiagnosticparser.ts
+++ src/diagnostic/qbsmsvcdiagnosticparser.ts
@@ -17,35 +17,37 @@ export class QbsMsvcDiagnosticParser extends QbsDiagnosticParser {
 
     private parseLine(line: string) {
         const matches = REGEX.exec(line.replace(/[\n\r]/g, ''));
-        if (matches) {
-            const [full, proc, file, location, severity, code, message] = matches;
-            const range = (() => {
-                const parts = location.split(',');
-                const n0 = QbsDiagnosticUtils.substractOne(parts[0]);
-                if (parts.length === 1) {
-                    return new vscode.Range(n0, 0, n0, 999);
-                } else if (parts.length === 2) {
-                    const n1 = QbsDiagnosticUtils.substractOne(parts[1]);
-                    return new vscode.Range(n0, n1, n0, n1);
-                } else if (parts.length === 4) {
-                    const n1 = QbsDiagnosticUtils.substractOne(parts[1]);
-                    const n2 = QbsDiagnosticUtils.substractOne(parts[2]);
-                    const n3 = QbsDiagnosticUtils.substractOne(parts[3]);
-                    return new vscode.Range(n0, n1, n2, n3);
-                }
-                throw new Error('Unable to determine location of MSVC diagnostic');
-            })();
-
-            const diagnostic: vscode.Diagnostic = {
-                source: this.type(),
-                message: message,
-                range: range,
-                severity: QbsMsvcDiagnosticParser.encodeSeverity(severity),
-                code: code
-            };
-
-            this.insertDiagnostic(file, diagnostic);
+        if (!matches) {
+            return;
         }
+
+        const [, , file, location, severity, code, message] = matches;
+        const range = (() => {
+            const parts = location.split(',');
+            const n0 = QbsDiagnosticUtils.substractOne(parts[0]);
+            if (parts.length === 1) {
+                return new vscode.Range(n0, 0, n0, 999);
+            } else if (parts.length === 2) {
+                const n1 = QbsDiagnosticUtils.substractOne(parts[1]);
+                return new vscode.Range(n0, n1, n0, n1);
+            } else if (parts.length === 4) {
+                const n1 = QbsDiagnosticUtils.substractOne(parts[1]);
+                const n2 = QbsDiagnosticUtils.substractOne(parts[2]);
+                const n3 = QbsDiagnosticUtils.substractOne(parts[3]);
+                return new vscode.Range(n0, n1, n2, n3);
+            }
+            throw new Error('Unable to determine location of MSVC diagnostic');
+        })();
+
+        const diagnostic: vscode.Diagnostic = {
+            source: this.type(),
+            severity: QbsMsvcDiagnosticParser.encodeSeverity(severity),
+            message,
+            range,
+            code
+        };
+
+        this.insertDiagnostic(file, diagnostic);
     }
 
     private static encodeSeverity(severity: string): vscode.DiagnosticSeverity {
