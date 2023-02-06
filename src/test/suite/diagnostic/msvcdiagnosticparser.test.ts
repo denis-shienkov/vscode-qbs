574821d4a3fe53b968e3cd38abdc2df1ad080784
Denis Shienkov <denis.shienkov@gmail.com>
2023-02-06 14:39:08 +0300

    tests: Add some diagnostic tests for MSVC compiler parser

--- src/test/suite/diagnostic/msvcdiagnosticparser.test.ts
+++ src/test/suite/diagnostic/msvcdiagnosticparser.test.ts
@@ -6,6 +6,110 @@ import { QbsToolchain } from '../../../protocol/qbsprotocolqbsmoduledata';
 
 suite('MSVC Diagnostic Parser', () => {
 
+    test('Parsing labeled compiler error', () => {
+        const parser = new QbsMsvcDiagnosticParser();
+        const lines = ['c:\\foo\\foo.hpp(67): error C1234: some error message'];
+        parser.parseLines(lines);
+
+        const collections = parser.getDiagnostics();
+        expect(collections.length).to.eq(1);
+        const collection = collections[0];
+        const uri = collection[0];
+        expect(uri.toString()).to.eq(vscode.Uri.file('c:\\foo\\foo.hpp').toString());
+        const diagnostics = collection[1];
+        expect(diagnostics.length).to.eq(1);
+        const diagnostic = diagnostics[0];
+        expect(diagnostic.source).to.eq(QbsToolchain.Msvc);
+        expect(diagnostic.severity).to.eq(vscode.DiagnosticSeverity.Error);
+        expect(diagnostic.message).to.eq('some error message');
+        expect(diagnostic.code).to.eq('C1234');
+        expect(diagnostic.range.start.line).to.eq(66);
+        expect(diagnostic.range.start.character).to.eq(0);
+        expect(diagnostic.range.end.line).to.eq(66);
+        expect(diagnostic.range.end.character).to.eq(999);
+
+        parser.clearDiagnistics();
+        expect(parser.getDiagnostics().length).to.eq(0);
+    });
+
+    test('Parsing labeled compiler error with number prefix', () => {
+        const parser = new QbsMsvcDiagnosticParser();
+        const lines = ['1>c:\\foo\\foo.hpp(67): error C1234: some error message : some details'];
+        parser.parseLines(lines);
+
+        const collections = parser.getDiagnostics();
+        expect(collections.length).to.eq(1);
+        const collection = collections[0];
+        const uri = collection[0];
+        expect(uri.toString()).to.eq(vscode.Uri.file('c:\\foo\\foo.hpp').toString());
+        const diagnostics = collection[1];
+        expect(diagnostics.length).to.eq(1);
+        const diagnostic = diagnostics[0];
+        expect(diagnostic.source).to.eq(QbsToolchain.Msvc);
+        expect(diagnostic.severity).to.eq(vscode.DiagnosticSeverity.Error);
+        expect(diagnostic.message).to.eq('some error message : some details');
+        expect(diagnostic.code).to.eq('C1234');
+        expect(diagnostic.range.start.line).to.eq(66);
+        expect(diagnostic.range.start.character).to.eq(0);
+        expect(diagnostic.range.end.line).to.eq(66);
+        expect(diagnostic.range.end.character).to.eq(999);
+
+        parser.clearDiagnistics();
+        expect(parser.getDiagnostics().length).to.eq(0);
+    });
+
+    test('Parsing labeled compiler warning', () => {
+        const parser = new QbsMsvcDiagnosticParser();
+        const lines = ['c:\\foo\\foo.hpp(67): warning C1234: some warning message : some details'];
+        parser.parseLines(lines);
+
+        const collections = parser.getDiagnostics();
+        expect(collections.length).to.eq(1);
+        const collection = collections[0];
+        const uri = collection[0];
+        expect(uri.toString()).to.eq(vscode.Uri.file('c:\\foo\\foo.hpp').toString());
+        const diagnostics = collection[1];
+        expect(diagnostics.length).to.eq(1);
+        const diagnostic = diagnostics[0];
+        expect(diagnostic.source).to.eq(QbsToolchain.Msvc);
+        expect(diagnostic.severity).to.eq(vscode.DiagnosticSeverity.Warning);
+        expect(diagnostic.message).to.eq('some warning message : some details');
+        expect(diagnostic.code).to.eq('C1234');
+        expect(diagnostic.range.start.line).to.eq(66);
+        expect(diagnostic.range.start.character).to.eq(0);
+        expect(diagnostic.range.end.line).to.eq(66);
+        expect(diagnostic.range.end.character).to.eq(999);
+
+        parser.clearDiagnistics();
+        expect(parser.getDiagnostics().length).to.eq(0);
+    });
+
+    test('Parsing labeled compiler warning with number prefix', () => {
+        const parser = new QbsMsvcDiagnosticParser();
+        const lines = ['1>c:\\foo\\foo.hpp(67): warning C1234: some warning message : some details'];
+        parser.parseLines(lines);
+
+        const collections = parser.getDiagnostics();
+        expect(collections.length).to.eq(1);
+        const collection = collections[0];
+        const uri = collection[0];
+        expect(uri.toString()).to.eq(vscode.Uri.file('c:\\foo\\foo.hpp').toString());
+        const diagnostics = collection[1];
+        expect(diagnostics.length).to.eq(1);
+        const diagnostic = diagnostics[0];
+        expect(diagnostic.source).to.eq(QbsToolchain.Msvc);
+        expect(diagnostic.severity).to.eq(vscode.DiagnosticSeverity.Warning);
+        expect(diagnostic.message).to.eq('some warning message : some details');
+        expect(diagnostic.code).to.eq('C1234');
+        expect(diagnostic.range.start.line).to.eq(66);
+        expect(diagnostic.range.start.character).to.eq(0);
+        expect(diagnostic.range.end.line).to.eq(66);
+        expect(diagnostic.range.end.character).to.eq(999);
+
+        parser.clearDiagnistics();
+        expect(parser.getDiagnostics().length).to.eq(0);
+    });
+
     test('Parsing pointed linker error', () => {
         const parser = new QbsMsvcDiagnosticParser();
         const lines = ['main.c.obj : error LNK1234: some error message'];
@@ -34,14 +138,14 @@ suite('MSVC Diagnostic Parser', () => {
 
     test('Parsing pointed linker fatal error', () => {
         const parser = new QbsMsvcDiagnosticParser();
-        const lines = ['c:\foo\bar\app.exe : fatal error LNK5678: some fatal error message'];
+        const lines = ['c:\\foo\\bar\\app.exe : fatal error LNK5678: some fatal error message'];
         parser.parseLines(lines);
 
         const collections = parser.getDiagnostics();
         expect(collections.length).to.eq(1);
         const collection = collections[0];
         const uri = collection[0];
-        expect(uri.toString()).to.eq(vscode.Uri.file('c:\foo\bar\app.exe').toString());
+        expect(uri.toString()).to.eq(vscode.Uri.file('c:\\foo\\bar\\app.exe').toString());
         const diagnostics = collection[1];
         expect(diagnostics.length).to.eq(1);
         const diagnostic = diagnostics[0];
